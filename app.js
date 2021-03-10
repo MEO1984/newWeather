9fe47a318162bbbe9951c7815cb582b0e3c6ed42
Mike O'Malley <MEO1984@gmail.com>
2021-03-09 19:26:36 -0500

    added button

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ window.addEventListener('load', ()=> {
     let temperatureDegree = document.querySelector('.temperature-degree');
     let timeZone = document.querySelector('.location-timezone');
     let temperatureDescription = document.querySelector('.temperature-description');
-    let icon = document.querySelector('.icon')
+    let icon = document.querySelector('.icon');
+    let button = document.querySelector(".change")
 
-
+// get weather info based off lcurr location
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(position => {
             long = position.coords.longitude;
@@ -19,11 +20,10 @@ window.addEventListener('load', ()=> {
                 return response.json();
             })
             .then(data => {
-                console.log(data);
-                temperatureDegree.textContent = data.main.temp;
+                rawTemp = data.main.temp;
+                temperatureDegree.textContent = Math.floor(rawTemp) + (`\xB0`) + " " + "F";
                 timeZone.textContent = data.name;
                 temperatureDescription.textContent = data.weather[0].main;
-                console.log(temperatureDescription);
                 iconDes = data.weather[0].icon;
                 iconUrl = "https://openweathermap.org/img/w/" + iconDes + ".png";
                 icon.src = iconUrl;
@@ -31,6 +31,27 @@ window.addEventListener('load', ()=> {
             })
         });
 
-    }
+    };
+    // get weather info based on input
+    button.addEventListener("click", function(){
+       var newCity= prompt("What city would you like to see?", "City Name");
+       const api = `https://api.openweathermap.org/data/2.5/weather?q=${newCity}&units=imperial&appid=1fe1b1156deb9197209e78927d7eff85`;
+
+       fetch(api)
+       .then(response => {
+           return response.json();
+       })
+       .then(data => {
+           console.log(data);
+           rawTemp = data.main.temp;
+           temperatureDegree.textContent = Math.floor(rawTemp) + (`\xB0`) + " " + "F";
+           timeZone.textContent = data.name;
+           temperatureDescription.textContent = data.weather[0].main;
+           iconDes = data.weather[0].icon;
+           iconUrl = "https://openweathermap.org/img/w/" + iconDes + ".png";
+           icon.src = iconUrl;
+           
+       })
+   });
     
 });
